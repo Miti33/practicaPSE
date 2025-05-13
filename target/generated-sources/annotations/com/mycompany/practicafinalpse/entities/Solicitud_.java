3ac486454923dde512f94f7cb45230e9dc8b814b
Davidlv04 <david.lopez.velasco22@estudiantes.uva.es>
2025-05-13 16:38:38 +0200

    admin arreglao

--- target/generated-sources/annotations/com/mycompany/practicafinalpse/entities/Solicitud_.java
+++ target/generated-sources/annotations/com/mycompany/practicafinalpse/entities/Solicitud_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2025-05-13T11:35:57")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2025-05-13T16:36:02")
 @StaticMetamodel(Solicitud.class)
 public class Solicitud_ { 
 
