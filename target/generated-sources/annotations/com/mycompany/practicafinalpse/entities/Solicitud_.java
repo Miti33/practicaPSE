4cee8df5890be47bdbf216a5bb1e29ca6867aa95
Davidlv04 <david.lopez.velasco22@estudiantes.uva.es>
2025-05-14 18:19:17 +0200

    MascotaRefugio Arreglao

--- target/generated-sources/annotations/com/mycompany/practicafinalpse/entities/Solicitud_.java
+++ target/generated-sources/annotations/com/mycompany/practicafinalpse/entities/Solicitud_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2025-05-14T17:58:49")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2025-05-14T18:12:28")
 @StaticMetamodel(Solicitud.class)
 public class Solicitud_ { 
 
