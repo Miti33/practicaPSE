4902a329f96799b416122d54468f33d2c018f947
Dimitar Vasilev Vasilev <dimitar.vasilev22@estudiantes.uva.es>
2025-05-14 11:59:32 +0200

    pagina inicio decnte

--- target/generated-sources/annotations/com/mycompany/practicafinalpse/entities/Cliente_.java
+++ target/generated-sources/annotations/com/mycompany/practicafinalpse/entities/Cliente_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2025-05-14T00:32:43")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2025-05-14T11:50:58")
 @StaticMetamodel(Cliente.class)
 public class Cliente_ { 
 
