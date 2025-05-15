fabe7c01f58273151b5193eab408b62ab32c9641
Davidlv04 <david.lopez.velasco22@estudiantes.uva.es>
2025-05-15 12:02:11 +0200

    chat para todos

--- target/generated-sources/annotations/com/mycompany/practicafinalpse/entities/Usuario_.java
+++ target/generated-sources/annotations/com/mycompany/practicafinalpse/entities/Usuario_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2025-05-15T09:25:09")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2025-05-15T12:01:48")
 @StaticMetamodel(Usuario.class)
 public class Usuario_ { 
 
